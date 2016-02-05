a607fc9a8acddb89c6d8f47db63894e4559fa9c7
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2016-02-05 14:14:57 +0300

    Fix StringUtilities::Split

--- runAs-tool-win32/JetBrains.runAs/StringUtilities.cpp
+++ runAs-tool-win32/JetBrains.runAs/StringUtilities.cpp
@@ -4,17 +4,19 @@
 std::vector<std::wstring> StringUtilities::Split(std::wstring &str, const std::wstring separator)
 {
 	std::vector<std::wstring> strs;
-	size_t i = 0;
-	auto pos = str.find(separator);
-	while (pos != std::wstring::npos)
+	auto separatorSize = separator.size();
+	size_t start = 0;
+	size_t pos;
+	while ((pos = str.find(separator, start)) != std::wstring::npos)
 	{
-		strs.push_back(str.substr(i, pos - i));
-		i = ++pos;
-		pos = str.find(separator, pos);
-		if (pos == std::string::npos)
-		{
-			strs.push_back(str.substr(i, str.length()));
-		}
+		strs.push_back(str.substr(start, pos - start - separatorSize));
+		start = pos + separatorSize;		
+	}
+
+	pos = str.size() - separatorSize;
+	if (start < pos)
+	{
+		strs.push_back(str.substr(start, pos));
 	}
 
 	return strs;
