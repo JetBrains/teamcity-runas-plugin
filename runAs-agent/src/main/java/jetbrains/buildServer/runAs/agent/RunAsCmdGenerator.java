31643d985017f95a4ca3d73c6607c30c9b90aa09
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2016-02-18 12:54:20 +0300

    Fix symbol esc for TeamCity service message from cmd echo

--- runAs-agent/src/main/java/jetbrains/buildServer/runAs/agent/RunAsCmdGenerator.java
+++ runAs-agent/src/main/java/jetbrains/buildServer/runAs/agent/RunAsCmdGenerator.java
@@ -1,5 +1,8 @@
 package jetbrains.buildServer.runAs.agent;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import jetbrains.buildServer.dotNet.buildRunner.agent.ResourceGenerator;
 import jetbrains.buildServer.messages.serviceMessages.Message;
 import org.jetbrains.annotations.NotNull;
@@ -7,6 +10,14 @@ import org.jetbrains.annotations.NotNull;
 public class RunAsCmdGenerator implements ResourceGenerator<RunAsCmdSettings> {
   private static final String LINE_SEPARATOR = System.getProperty("line.separator");
   private static final String NORMAL_STATUS = "NORMAL";
+  private static final List<Replacement> OurReplacements = Collections.unmodifiableList(Arrays.asList(
+    new Replacement("^", "^^"),
+    new Replacement("\\", "^\\"),
+    new Replacement("&", "^&"),
+    new Replacement("|", "^|"),
+    new Replacement("'", "^'"),
+    new Replacement(">", "^>"),
+    new Replacement("<", "^<")));
 
   @NotNull
   @Override
@@ -21,11 +32,10 @@ public class RunAsCmdGenerator implements ResourceGenerator<RunAsCmdSettings> {
 
     sb.append(LINE_SEPARATOR);
     sb.append("ECHO ");
-    sb.append(new Message("Starting: " + settings.getCommandLine(), NORMAL_STATUS, null).toString());
-
+    sb.append(ApplyCmdStringReplacements(new Message("Starting: " + settings.getCommandLine(), NORMAL_STATUS, null).toString()));
     sb.append(LINE_SEPARATOR);
     sb.append("ECHO ");
-    sb.append(new Message("in directory: " + settings.getWorkingDirectory(), NORMAL_STATUS, null).toString());
+    sb.append(ApplyCmdStringReplacements(new Message("in directory: " + settings.getWorkingDirectory(), NORMAL_STATUS, null).toString()));
 
     sb.append(LINE_SEPARATOR);
     sb.append(settings.getCommandLine());
@@ -40,5 +50,37 @@ public class RunAsCmdGenerator implements ResourceGenerator<RunAsCmdSettings> {
     sb.append("EXIT /B %EXIT_CODE%");
 
     return sb.toString();
+  }
+
+  @NotNull
+  static String ApplyCmdStringReplacements(@NotNull final String text)
+  {
+    String curText = text;
+    for (Replacement replacement: OurReplacements) {
+      curText = curText.replace(replacement.getTargetString(), replacement.getReplacementString());
+    }
+
+    return curText;
+  }
+
+  static class Replacement
+  {
+    private final String myTargetString;
+    private final String myReplacementString;
+
+    public Replacement(@NotNull final String targetString, @NotNull final String replacementString) {
+      myTargetString = targetString;
+      myReplacementString = replacementString;
+    }
+
+    @NotNull
+    public String getTargetString() {
+      return myTargetString;
+    }
+
+    @NotNull
+    public String getReplacementString() {
+      return myReplacementString;
+    }
   }
 }
--- runAs-agent/src/test/java/jetbrains/buildServer/runAs/agent/RunAsCmdGeneratorTest.java
+++ runAs-agent/src/test/java/jetbrains/buildServer/runAs/agent/RunAsCmdGeneratorTest.java
@@ -1,24 +1,50 @@
 package jetbrains.buildServer.runAs.agent;
 
 import org.jetbrains.annotations.NotNull;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
 public class RunAsCmdGeneratorTest {
+  private static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
-  @Test()
-  public void shouldGenerateContent() {
+  @DataProvider(name = "cmdLinesCases")
+  public Object[][] getCmdLinesCases() {
+    return new Object[][] {
+      { "cmd line", "cmd line" },
+      { "^", "^^" },
+      { "^^", "^^^^" },
+      { "\\", "^\\" },
+      { "&", "^&" },
+      { "|", "^|^|" },
+      { "\\", "^\\" },
+      { "'", "^|^'" },
+      { ">", "^>" },
+      { "<", "^<" },
+      { "cmd line where cat!=OOMT&&cat!=OOM", "cmd line where cat!=OOMT^&^&cat!=OOM" },
+      { "cmd line where cat!='OOMT&&cat!=OOM'", "cmd line where cat!=^|^'OOMT^&^&cat!=OOM^|^'" },
+      { "'\\&|><^'", "^|^'^\\^&^|^|^>^<^^^|^'" },
+    };
+  }
+
+  @Test(dataProvider = "cmdLinesCases")
+  public void shouldGenerateContent(@NotNull final String cmdLine, @NotNull final String cmdLineInMessage) {
     // Given
     final RunAsCmdGenerator instance = createInstance();
 
     // When
-    final String content = instance.create(new RunAsCmdSettings("cmd line", "w d"));
+    final String content = instance.create(new RunAsCmdSettings(cmdLine, "w d"));
 
     // Then
-    then(content).contains("PUSHD \"w d\"");
-    then(content).contains("cmd line");
-    then(content).contains("POPD");
+    then(content).isEqualTo("@ECHO OFF"
+                            + LINE_SEPARATOR + "PUSHD \"w d\""
+                            + LINE_SEPARATOR + "ECHO ##teamcity[message text=^'Starting: " + cmdLineInMessage + "^' status=^'NORMAL^']"
+                            + LINE_SEPARATOR + "ECHO ##teamcity[message text=^'in directory: w d^' status=^'NORMAL^']"
+                            + LINE_SEPARATOR + cmdLine
+                            + LINE_SEPARATOR + "SET \"EXIT_CODE=%ERRORLEVEL%\""
+                            + LINE_SEPARATOR + "POPD"
+                            + LINE_SEPARATOR + "EXIT /B %EXIT_CODE%");
   }
 
   @NotNull
