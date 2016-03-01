e3f52d4f658829bf5281b32815a3fdd16a36a730
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2016-03-01 13:28:10 +0300

    Add build proj

--- build.cmd
+++ build.cmd
@@ -1,7 +1,6 @@
-call msbuild runAs-tool-win32\tools.sln /p:Configuration=Release /p:Platform="x86"
-runAs-tool-win32\packages\NUnit.Console.3.0.1\tools\nunit3-console.exe C:\Projects\GitHub\JetBrains\teamcity-runas\runAs-tool-win32\bin\IntegrationTests\Release\JetBrains.runAs.IntegrationTests.dll
-call msbuild runAs-tool-win32\tools.sln /p:Configuration=Release /p:Platform="x64"
-runAs-tool-win32\packages\NUnit.Console.3.0.1\tools\nunit3-console.exe C:\Projects\GitHub\JetBrains\teamcity-runas\runAs-tool-win32\bin\IntegrationTests\Release\JetBrains.runAs.IntegrationTests.dll
+msbuild runAs-tool-win32\build.proj
 REM call msbuild runAs-tool\runAs-tool.sln /p:Configuration=Release
+
 call mvn package
+
 call createPlugin.cmd
