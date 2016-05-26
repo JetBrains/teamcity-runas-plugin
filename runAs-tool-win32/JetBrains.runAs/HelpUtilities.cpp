ea95620b68ad9249614a98b50c9d5cff0c10e798
Julia-Alexandrova <Julia-Alexandrova@users.noreply.github.com>
2016-05-26 16:30:13 +0300

    some text fixes

--- runAs-tool-win32/JetBrains.runAs/HelpUtilities.cpp
+++ runAs-tool-win32/JetBrains.runAs/HelpUtilities.cpp
@@ -42,45 +42,45 @@ wstring HelpUtilities::GetHelp()
 	wstringstream help;
 	help << endl << L"Usage:";
 	help << endl << L"JetBrains.runAs.exe arguments";
-	help << endl << L"where arguments are:";
+	help << endl << L"where the arguments are:";
 	
 	help << endl << L"\t-u:" << ARG_USER_NAME << "\t\t- \"user\" or \"domain\\user\" or \"user@domain\".";
 	
-	help << endl << L"\t-p:" << ARG_PASSWORD << "\t\t- user's password, it is optional and empty by default.";
+	help << endl << L"\t-p:" << ARG_PASSWORD << "\t\t- user's password, optional and empty by default.";
 	
-	help << endl << L"\t-w:" << ARG_WORKING_DIRECTORY << "\t- current directory, it is optional and empty by default.";
+	help << endl << L"\t-w:" << ARG_WORKING_DIRECTORY << "\t- current directory,  optional and empty by default.";
 	
-	help << endl << L"\t-b:" << ARG_EXIT_CODE_BASE << "\t- base number for exit code, it is optional and " << EXIT_CODE_BASE << L" by default.";
+	help << endl << L"\t-b:" << ARG_EXIT_CODE_BASE << "\t- base number for exit code, optional and " << EXIT_CODE_BASE << L" by default.";
 	
 	help << endl << L"\t-e:" << ARG_ENV_VAR << "\t- set an environment variable in the format \"name=value\".";
 	
-	help << endl << L"\t-l:" << ARG_LOG_LEVEL << "\t\t- logging level (" << StringUtilities::Join(LogLevels, L"|") << L"), it is optional and \"" << LOG_LEVEL_NORMAL << L"\" by default.";
+	help << endl << L"\t-l:" << ARG_LOG_LEVEL << "\t\t- logging level (" << StringUtilities::Join(LogLevels, L"|") << L"), optional and \"" << LOG_LEVEL_NORMAL << L"\" by default.";
 	
-	help << endl << L"\t-il:" << ARG_INTEGRITY_LEVEL << "\t- integrity level (" << StringUtilities::Join(IntegrityLevels, L"|") << L"), it is optional and \"" << INTEGRITY_LEVEL_DEFAULT << L"\" by default.";
+	help << endl << L"\t-il:" << ARG_INTEGRITY_LEVEL << "\t- integrity level (" << StringUtilities::Join(IntegrityLevels, L"|") << L"), optional and \"" << INTEGRITY_LEVEL_DEFAULT << L"\" by default.";
 	
-	help << endl << L"\t-s:" << ARG_SHOW_MODE << "\t\t- show mode (" << StringUtilities::Join(ShowModes, L"|") << L"), it is optional and \"" << SHOW_MODE_HIDE << L"\" by default.";
+	help << endl << L"\t-s:" << ARG_SHOW_MODE << "\t\t- show mode (" << StringUtilities::Join(ShowModes, L"|") << L"), optional and \"" << SHOW_MODE_HIDE << L"\" by default.";
 	
-	help << endl << L"\t-t" << "\t\t\t- self testing. Returns exit codes:";
-	help << endl << L"\t\t32\t\t\tready to work on in the 32-bit mode";
-	help << endl << L"\t\t64\t\t\tready to work on in the 64-bit mode-";	
-	help << endl << L"\t\t" << EXIT_CODE_NO_ADMIN << L"\t\t\tinvoker has no administrative privileges, when running under the Windows service";	
-	help << endl << L"\t\t" << EXIT_CODE_NO_ASSIGN_PRIMARY_TOKEN_PRIV << L"\t\t\tinvoker has no SeAssignPrimaryTokenPrivilege privilege, when running under the Windows service";
+	help << endl << L"\t-t" << "\t\t\t- self-testing. Returns exit codes:";
+	help << endl << L"\t\t32\t\t\tready to work in the 32-bit mode";
+	help << endl << L"\t\t64\t\t\tready to work in the 64-bit mode-";	
+	help << endl << L"\t\t" << EXIT_CODE_NO_ADMIN << L"\t\t\tinvoker has no administrative privileges when running under the Windows service";	
+	help << endl << L"\t\t" << EXIT_CODE_NO_ASSIGN_PRIMARY_TOKEN_PRIV << L"\t\t\tinvoker has no SeAssignPrimaryTokenPrivilege privilege when running under the Windows service";
 	
 	help << endl << L"\t-i:" << ARG_INHERITANCE_MODE << "\t- inheritance mode for the creating process, optional and \"" << INHERITANCE_MODE_AUTO << L"\" by default.";
-	help << endl << L"\t\t" << INHERITANCE_MODE_ON << L"\t\t\tthe environment variables should be inherited from a parent process";
-	help << endl << L"\t\t" << INHERITANCE_MODE_AUTO << L"\t\t\tthe some environment variables should be inherited from a parent process";
-	help << endl << L"\t\t" << INHERITANCE_MODE_OFF << L"\t\t\tenvironment variables should not be inherited from a parent process";
+	help << endl << L"\t\t" << INHERITANCE_MODE_ON << L"\t\t\tenvironment variables will be inherited from a parent process";
+	help << endl << L"\t\t" << INHERITANCE_MODE_AUTO << L"\t\t\tsome environment variables will be inherited from a parent process";
+	help << endl << L"\t\t" << INHERITANCE_MODE_OFF << L"\t\t\tenvironment variables will not be inherited from a parent process";
 	
-	help << endl << L"\t-c:" << ARG_CONFIGURATION_FILE << L"\t- text file, containing the any configuration arguments, it is optional.";
+	help << endl << L"\t-c:" << ARG_CONFIGURATION_FILE << L"\t- text file containing configuration arguments, optional.";
 	
 	help << endl << L"\t" << ARG_EXECUTABLE << "\t\t- executable file.";
 	
-	help << endl << L"\t" << ARG_EXIT_COMMAND_LINE_ARGS << "\t- command line arguments, it is optional and empty by default. The maximum total length of \"" << ARG_EXECUTABLE << L"\" and \"" << ARG_EXIT_COMMAND_LINE_ARGS << L"\" is 1024 characters.";
+	help << endl << L"\t" << ARG_EXIT_COMMAND_LINE_ARGS << "\t- command line arguments, optional and empty by default. The maximum total length of \"" << ARG_EXECUTABLE << L"\" and \"" << ARG_EXIT_COMMAND_LINE_ARGS << L"\" is 1024 characters.";
 	
 	help << endl;
 	help << endl << L"Example:";
 	help << endl << L"JetBrains.runAs.exe -p:MyPassword -c:MyConfig.txt";
-	help << endl << L"where \"MyConfig.txt\" is a text file containing following lines:";
+	help << endl << L"where \"MyConfig.txt\" is a text file containing the following lines:";
 	help << endl << L"\t-u:SomeDomain\\SomeUserName";
 	help << endl << L"\tWhoAmI.exe";
 	help << endl << L"\t/ALL";
