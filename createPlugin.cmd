c3b4f82977cd5baf9aace5120d177fb7a3135dd8
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2016-06-24 16:37:46 +0300

    Configure build

--- createPlugin.cmd
+++ createPlugin.cmd
@@ -7,11 +7,11 @@ rd %root%plugin /s /q
 %zip% x %root%target\runAs.zip -o%root%plugin
 
 md %root%plugin\agent\new\bin
-copy %root%runAs-tool-win32\cmd\*.cmd %root%plugin\agent\new\bin /Y
+copy %root%cmd\*.cmd %root%plugin\agent\new\bin /Y
 md %root%plugin\agent\new\bin\x86
-copy %root%runAs-tool-win32\bin\Release\x86\JetBrains.runAs.exe %root%plugin\agent\new\bin\x86 /Y
+copy %root%win32\x64\JetBrains.runAs.exe %root%plugin\agent\new\bin\x86 /Y
 md %root%plugin\agent\new\bin\x64
-copy %root%runAs-tool-win32\bin\Release\x64\JetBrains.runAs.exe %root%plugin\agent\new\bin\x64 /Y
+copy %root%win32\x64\JetBrains.runAs.exe %root%plugin\agent\new\bin\x64 /Y
 
 pushd plugin\agent\new
 %zip% a -tzip %root%plugin\agent\runAs-agent.zip *
