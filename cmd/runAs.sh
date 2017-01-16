c2185b36c8c98c1c27a539642ede55284b6f984d
Nikolay.Pianikov <NikolayP@live.com>
2017-01-16 14:32:15 +0300

    #20 Couldn't start a build under runAs user on linux agent - fix end of lines for .sh and .cmd

--- cmd/runAs.sh
+++ cmd/runAs.sh
@@ -5,67 +5,68 @@ if [ $# -eq 4 ];
 then
 	args=$(cat "$1")
 	command="$2"
-        password="$4"
+	password="$4"
 
+	# if root
+	if [[ "$EUID" -eq 0 ]];
+	then		
+		su -c $command $args
+		exit $?
+	fi
+
+	# if not root
 	${0} runAs "$args" "$command" "$password" arg5
-
 	exit $?
 fi
 
-#  runAs (runAs, args, command, password)
+#  runAs (runAs, args, command, password, arg5)
 if [ $# -eq 5 ];
 then
 	if [ "$1" = "runAs" ];
 	then
-        	args="\"$2\""
-	        command="$3"
-        	password="$4"
+		args="\"$2\""
+		command="$3"
+		password="$4"
 
 		tmpFile=$(tempfile)
-        	chmod a+rw "$tmpFile"
+		chmod a+rw "$tmpFile"
 
 		cmd="${0} su $tmpFile $command $args arg5"
 
-        	# run command
-		if [[ "$EUID" -eq 0 ]];
-        	then
-			# if root
-	                $cmd
-		else
-		        (
-				# wait for password input
-				attempts=50
-				while [[ ! -s "$tmpFile" || attemps -gt 0 ]];
-				do
-					sleep .1
-					attempts=$((attempts-1))
-				done
+		# run command
+		(
+			# wait for password input
+			attempts=50
+			while [[ ! -s "$tmpFile" || attemps -gt 0 ]];
+			do
+				sleep .1
+				attempts=$((attempts-1))
+			done
 
-				if [[ $attempts -eq 0 ]];
-				then
-					echo "##teamcity[message text='Error during sending password.' status='ERROR']"
-					exit 255
-				fi
+			if [[ $attempts -eq 0 ]];
+			then
+				echo "##teamcity[message text='Error during sending password.' status='ERROR']"
+				exit 255
+			fi
 
-				# send password to su stdIn
-				echo "$password"
+			# send password to su stdIn
+			echo "$password"
 
-				# wait for process finish
-				while ps axg | grep "$tmpFile" > /dev/null; 
-				do
-					sleep .1
-				done
-			) | (
-				# su
-				socat - EXEC:"$cmd",pty,ctty,setsid;
-			) 2> >(tee > "$tmpFile" >(grep -v "[Pp]assword:" >&2))
-		fi
+			# wait for process finish
+			while ps axg | grep "$tmpFile" | grep -v "grep" > /dev/null;
+			do
+				sleep .1
+			done
+		) | (
+			# su
+			socat - EXEC:"$cmd",pty,ctty,setsid;
+		) 2> >(tee > "$tmpFile" >(grep -v "[Pp]assword:" >&2))
 
-	        # if exit file is empty
-        	if [ ! -s "$tmpFile" ];
-	        then
+		# if exit file is empty
+		if [ ! -s "$tmpFile" ];
+		then
 			exit 255
-	        fi
+		fi
 
 		# take exid code from file
 		exitCode=$(cat "$tmpFile")
@@ -73,13 +74,14 @@ then
 		exit $exitCode
 	fi
 
-	# su (su, tmp_file, command, args)
+	# su (su, tmp_file, command, args, arg5)
 	if [ "$1" = "su" ];
 	then
 		tmpFile=$2
 		command=$3
 		args=$4
 
+		echo su -c "$command" "$args"
 		su -c $command $args
 		exitCode=$?
 
